784f72f68d24d36da655c92aa95df49c27f76253
ipd <la24vue@gmail.com>
2020-11-12 13:54:10 +0000

    validation, DRY, new column added (status)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :identify_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
   
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -24,11 +25,9 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
     
     if @task.update(task_params)
       flash[:success] = "タスクID:#{@task.id}を修正しました。"
@@ -40,7 +39,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    
     @task.destroy
     
     flash[:success] = 'タスクは削除されました。'
@@ -51,8 +50,12 @@ class TasksController < ApplicationController
   
   private
   
+  def identify_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
   
   
