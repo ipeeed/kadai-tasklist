4f22fd9d2c62a1e5ce9913ba2c3f4b6784363357
ipd <la24vue@gmail.com>
2020-11-24 16:23:06 +0000

    developed user-login system

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :identify_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_login
   
   def index
     @tasks = Task.all.page(params[:page]).per(2)
@@ -13,7 +14,7 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'タスクが登録されました。'
