bd0b7967bd4dcd3d24fdfe1ebd362fa5d1d68ac8
ipd <la24vue@gmail.com>
2020-11-26 13:20:02 +0000

    modified for submitting

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   before_action :require_login
-  before_action :identify_task, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:show, :edit, :update, :destroy]
 
   
@@ -53,18 +52,16 @@ class TasksController < ApplicationController
   
   private
   
-  def identify_task
-    @task = Task.find(params[:id])
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
   
   def task_params
     params.require(:task).permit(:content, :status)
   end
   
-  def correct_user
-    unless @task.user.id == current_user.id
-      redirect_to root_url
-    end
-  end
   
 end
